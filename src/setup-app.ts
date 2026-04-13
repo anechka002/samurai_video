e934fb236f8436038c5ca42af8dad1b7c29a1488
Anna <blinova.anneta@gmail.com>
2026-04-13 20:40:38 +0200

    fix url

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,9 +1,13 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import {videosRouter} from "./videos/routers/video.router";
 import {testingRouter} from "./testing/routers/testing.router";
 
 export const setupApp = (app: Express) => {
   app.use(express.json()); // middleware для парсинга JSON в теле запроса
+
+  app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('hello world!!!');
+  });
 
   // Подключаем роутеры
   app.use('/hometask_01/api/videos', videosRouter);
